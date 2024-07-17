275fe15d755e4ecc962eb4751d1faf655d01f598
amitlandge <amitlandge333@gmail.com>
2024-07-17 18:30:22 +0530

    some changes

--- src/context/socketContext.jsx
+++ src/context/socketContext.jsx
@@ -7,6 +7,9 @@ const SocketProvider = (prop) => {
   const socket = useMemo(() => {
     return io(server, { withCredentials: true });
   }, []);
+  socket.on("error", (error) => {
+    console.error("Socket.IO error:", error);
+  });
   return (
     <SocketContext.Provider value={socket}>
       {prop.children}
